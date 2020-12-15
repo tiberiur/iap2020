24988c5869b75d0f1d0861a8284c7db8cb461627
Tiberiu Rus <ro.tiberiu@gmail.com>
2020-12-15 15:40:28 +0200

    added feature for first/pinned post

--- wp-content/themes/iap/home.php
+++ wp-content/themes/iap/home.php
@@ -1,13 +1,26 @@
 <?php get_header(); ?>
 <main class="main-home">
+
+    <?php $featuredPost = new WP_Query(array('posts_per_page'=>'1')) ?>
+    <?php if($featuredPost->have_posts()): ?>
+        <div class="featured-post">
+        <?php while($featuredPost->have_posts()): ?>
+            <?php $featuredPost->the_post() ?>
+            <?php get_template_part( 'template-parts/content', 'post' ); ?>
+        <?php endwhile ?>
+        </div>
+    <?php endif ?>
+
     <?php if ( have_posts() ): ?>
         <h2>All articles</h2>
         <div class="posts">
-            <?php while ( have_posts() ):?>
+            <?php $i = 0; while ( have_posts() ): ?>
                 <?php the_post(); ?>
-                <?php get_template_part( 'template-parts/content', 'post' ); ?>
-            <?php endwhile ?>
+                <?php if($i !== 0): // skip the first post ?>
+                    <?php get_template_part( 'template-parts/content', 'post' ); ?>
+                <?php endif ?>
+            <?php $i++; endwhile ?>
         </div>
-    <?php endif; ?>
+    <?php endif ?>
 </main>
 <?php get_footer(); ?>
